2f8c9619c5a6982e7d39a5fab242e701a482f235
Fama Dieme <fasalitouba10@gmail.com>
2024-05-28 14:03:28 +0200

    show, root sur index et delete

--- app/controllers/elves_controller.rb
+++ app/controllers/elves_controller.rb
@@ -1,4 +1,8 @@
 class ElvesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  before_action :set_elf, only: [:show, :destroy]
+
   def index
     @elves = Elf.all
   end
@@ -13,5 +17,13 @@ class ElvesController < ApplicationController
   end
 
   def destroy
+    @elf.destroy
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def set_elf
+    @elf = Elf.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
-  get 'elves/index'
-  get 'elves/show'
-  get 'elves/new'
   devise_for :users
-  root to: "pages#home"
+  root to: "elves#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :elves, only: [:index, :show, :new, :create, :delete]
-  
+  resources :elves, only: [:index, :show, :new, :create, :destroy]
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
