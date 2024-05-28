ea395266843c31959453f04cd16345273415ed68
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-05-28 18:03:06 +0200

    create route controller vue booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,14 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :elf
+
+  before_save :compute_total_price
+
+  def compute_total_price
+    self.total_price = elf.daily_price * duration_in_days
+  end
+
+  def duration_in_days
+    (end_date - start_date).to_i
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "elves#index"
-  
-  resources :elves, only: %w[index show new create destroy]
+
+  resources :elves, only: %w[index show new create destroy] do
+    resources :bookings, only: %w[new create]
+  end
+  resources :bookings, only: %w[index destroy]
 end
