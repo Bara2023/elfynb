9adad31cfecdb2072271364b528abf2f741693be
Benoit LEDEUIL <benoitledeuil@gmail.com>
2024-05-29 18:03:16 +0200

    pouvoir editer et update un elf

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "elves#index"
 
-  resources :elves, only: %w[index show new create destroy] do
+  resources :elves, only: %w[index show new create edit update destroy] do
     resources :bookings, only: %w[new create]
   end
   resources :bookings, only: %w[index destroy]
