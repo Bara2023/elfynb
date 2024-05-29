207b15b94e06f33305238efcb5b416898b6f625c
Gordon GIRY <giry.gordon1@gmail.com>
2024-05-29 15:17:06 +0200

    add address seed for mapbox

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_29_095520) do
     t.text "description"
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
     t.index ["user_id"], name: "index_elves_on_user_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,27 +11,27 @@ romain = User.create(first_name: "Romain", last_name: "Chevallier ", phone_numbe
 benoit = User.create(first_name: "Benoit", last_name: "Dcd", phone_number: "0606066063", email: "benoit@lewagon.com", password: "123456")
 gordon = User.create(first_name: "Gordon", last_name: "Freeman", phone_number: "0606066064", email: "gordon@lewagon.com", password: "123456")
 
-dobby = Elf.new(name: "Dobby", category: "Brave", age: "122", daily_price: "4", user: paul, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?")
+dobby = Elf.new(name: "Dobby", category: "Brave", age: "122", daily_price: "4", user: paul, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "6 allée du cormier")
 file = URI.open('app/assets/images/dobby.png')
 dobby.photo.attach(io: file, filename: "dobby.png", content_type: "image/png")
 dobby.save
 
-kreattur = Elf.new(name: "Kreattur", category: "NRV", age: "14", daily_price: "10", user: fama, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?")
+kreattur = Elf.new(name: "Kreattur", category: "NRV", age: "14", daily_price: "10", user: fama, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "2 rue de la paix")
 file = URI.open('app/assets/images/kreacher.png')
 kreattur.photo.attach(io: file, filename: "kreattur.png", content_type: "image/png")
 kreattur.save
 
-winky = Elf.new(name: "Winky", category: "Jardinage", age: "42", daily_price: "15", user: romain, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?")
+winky = Elf.new(name: "Winky", category: "Jardinage", age: "42", daily_price: "15", user: romain, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "12 rue de passy")
 file = URI.open('app/assets/images/winky.jpeg')
 winky.photo.attach(io: file, filename: "winky.jpeg", content_type: "image/jpeg")
 winky.save
 
-hokey = Elf.new(name: "Hokey", category: "Sportif", age: "5", daily_price: "4", user: benoit, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?")
+hokey = Elf.new(name: "Hokey", category: "Sportif", age: "5", daily_price: "4", user: benoit, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "5 place des victoires")
 file = URI.open('app/assets/images/hokey.jpeg')
 hokey.photo.attach(io: file, filename: "hokey.jpeg", content_type: "image/jpeg")
 hokey.save
 
-monique = Elf.new(name: "Monique", category: "Artiste", age: "500", daily_price: "30", user: gordon, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?")
+monique = Elf.new(name: "Monique", category: "Artiste", age: "500", daily_price: "30", user: gordon, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "52 avenue de l'opéra")
 file = URI.open('app/assets/images/monique.jpeg')
 monique.photo.attach(io: file, filename: "monique.jpeg", content_type: "image/jpeg")
 monique.save
