881eccbf9e98a91327befea3a24d971c2c0b3534
Gordon GIRY <giry.gordon1@gmail.com>
2024-05-29 17:44:01 +0200

    add category, update seed category

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,22 @@ romain = User.create(first_name: "Romain", last_name: "Chevallier ", phone_numbe
 benoit = User.create(first_name: "Benoit", last_name: "Dcd", phone_number: "0606066063", email: "benoit@lewagon.com", password: "123456")
 gordon = User.create(first_name: "Gordon", last_name: "Freeman", phone_number: "0606066064", email: "gordon@lewagon.com", password: "123456")
 
-dobby = Elf.new(name: "Dobby", category: "Brave", age: "122", daily_price: "4", user: paul, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "6 allée du cormier")
+dobby = Elf.new(name: "Dobby", category: "Artiste", age: "122", daily_price: "4", user: paul, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "6 allée du cormier")
 file = URI.open('app/assets/images/dobby.png')
 dobby.photo.attach(io: file, filename: "dobby.png", content_type: "image/png")
 dobby.save
 
-kreattur = Elf.new(name: "Kreattur", category: "NRV", age: "14", daily_price: "10", user: fama, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "2 rue de la paix")
+kreattur = Elf.new(name: "Kreattur", category: "Jardinier", age: "14", daily_price: "10", user: fama, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "2 rue de la paix")
 file = URI.open('app/assets/images/kreacher.png')
 kreattur.photo.attach(io: file, filename: "kreattur.png", content_type: "image/png")
 kreattur.save
 
-winky = Elf.new(name: "Winky", category: "Jardinage", age: "42", daily_price: "15", user: romain, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "12 rue de passy")
+winky = Elf.new(name: "Winky", category: "Chauffeur", age: "42", daily_price: "15", user: romain, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "12 rue de passy")
 file = URI.open('app/assets/images/winky.jpeg')
 winky.photo.attach(io: file, filename: "winky.jpeg", content_type: "image/jpeg")
 winky.save
 
-hokey = Elf.new(name: "Hokey", category: "Sportif", age: "5", daily_price: "4", user: benoit, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "5 place des victoires")
+hokey = Elf.new(name: "Hokey", category: "Coach Sportif", age: "5", daily_price: "4", user: benoit, description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi iure vel illum velit similique corrupti doloremque, ratione reiciendis, aspernatur ea officia saepe sed qui veritatis commodi mollitia ex nihil?", address: "5 place des victoires")
 file = URI.open('app/assets/images/hokey.jpeg')
 hokey.photo.attach(io: file, filename: "hokey.jpeg", content_type: "image/jpeg")
 hokey.save
