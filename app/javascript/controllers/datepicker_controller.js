083eebe4ba9cfc35017f8dea891eb7f0b06e1ffd
Gordon GIRY <giry.gordon1@gmail.com>
2024-05-30 14:53:10 +0200

    add datepicker disable

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -4,9 +4,26 @@ import flatpickr from "flatpickr"; // You need to import this to use new flatpic
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
+  static values = { dates: Object }
+
   connect() {
-    flatpickr(this.element, {
-      minDate: Date.now()
-     })
+    this.options = {
+      minDate: Date.now(),
+    }
+    this.disableDates();
+    flatpickr(this.element, this.options)
+  }
+
+  disableDates() {
+    console.log(this.datesValue);
+    if (this.datesValue.disable.length > 0) {
+      this.options = {
+        ...this.options,
+        ...this.datesValue
+      }
+    } else {
+      this.options
+    }
+    console.log(this.options);
   }
 }
--- app/models/elf.rb
+++ app/models/elf.rb
@@ -5,10 +5,31 @@ class Elf < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
+  has_many :bookings
+
   validates :name, presence: true, uniqueness: true
   validates :age, presence: true, numericality: { only_integer: true }
   validates :category, presence: true
   validates :daily_price, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
-  # validates :photo, presence: true
+
+  def booked_dates
+    bookings.pluck(:start_date, :end_date)
+  end
+
+  def booked_dates_hashes
+    booked_dates.map do |start_date, end_date|
+      {
+        from: start_date.to_s,
+        to: end_date.to_s
+      }
+    end
+  end
+
+  def flatpickr_booked_dates
+    {
+      dateFormat: "Y-m-d",
+      disable: booked_dates_hashes
+    }.to_json
+  end
 end
